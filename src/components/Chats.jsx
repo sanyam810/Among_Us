23d3aeef7335714531aa9d468a82a62a67716633
sanyam810 <sanyamsaini081@gmail.com>
2023-04-05 16:12:19 +0530

    working to fetch chat conversations

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,40 +1,39 @@
-import React from 'react'
-
+import React, { useState, useEffect, useContext } from 'react';
+import { onSnapshot, doc } from "firebase/firestore";
+import { db } from "../firebase";
+import { AuthContext } from "../Context/AuthContext";
 export const Chats = () => {
+
+ const[chats,setChats]=useState([])
+ const {currentUser}=useContext(AuthContext)
+
+ useEffect(()=>{
+  const getChats=()=>{
+    const unsub=onSnapshot(doc(db,"userChats",currentUser.uid),doc=>{
+      setChats(doc.data())
+    });
+    return()=>{
+      unsub();
+    };
+  };
+
+  currentUser.uid && getChats()
+ },[currentUser.uid]);
+
   return (
     <div className="Chats">
-      <div className="userChat">
-        <img 
-          src="https://images.pexels.com/photos/9024329/pexels-photo-9024329.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" 
-          alt=""
-        />
-        <div className="userChatInfo">
-          <span>Jane</span>
-          <p>Hello</p>
+      {Object.entries(chats)?.map(chat=>(
+        <div className="userChat" key={chat[0]}>
+          <img 
+            src={chat[1].userInfo.photoURL} alt=""/>
+          <div className="userChatInfo">
+            <span>{chat[1].userInfo.displayName}</span>
+            <p>{chat[1].userInfo.lastMessage?.text}</p>
+          </div>
         </div>
-      </div>
-      <div className="userChat">
-        <img 
-          src="https://images.pexels.com/photos/9024329/pexels-photo-9024329.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" 
-          alt=""
-        />
-        <div className="userChatInfo">
-          <span>Jane</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      <div className="userChat">
-        <img 
-          src="https://images.pexels.com/photos/9024329/pexels-photo-9024329.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" 
-          alt=""
-        />
-        <div className="userChatInfo">
-          <span>Jane</span>
-          <p>Hello</p>
-        </div>
-      </div>
+      ))}
     </div>
-  )
+  );
 }
 
 export default Chats;
