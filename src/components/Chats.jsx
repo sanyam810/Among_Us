c19804495b6bb77a792ae630ec45aa077273bef1
sanyam810 <sanyamsaini081@gmail.com>
2023-04-07 02:28:59 +0530

    a random hey is apperaing

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -24,21 +24,27 @@ const Chats = () => {
     currentUser.uid && getChats();
   }, [currentUser.uid]);
 
-  const handleSelect = (u) => {
-    dispatch({ type: "CHANGE_USER", payload: u });
+  const handleSelect = (userInfo) => {
+    if (userInfo && userInfo.uid){
+      const user = { ...userInfo, uid: userInfo.uid || "" };
+      dispatch({ type: "CHANGE_USER", payload: user });
+    }
+    
   };
 
   return (
     <div className="chats">
+      
       {Object.entries(chats)?.sort((a,b)=>b[1].date - a[1].date).map((chat) => (
+        
         <div
           className="userChat"
           key={chat[0]}
           onClick={() => handleSelect(chat[1].userInfo)}
         >
-          <img src={chat[1].userInfo.photoURL} alt="" />
+          <img src={chat[1].userInfo?.photoURL} alt="" />
           <div className="userChatInfo">
-            <span>{chat[1].userInfo.displayName}</span>
+            <span>{chat[1].userInfo?.displayName}</span>
             <p>{chat[1].lastMessage?.text}</p>
           </div>
         </div>
